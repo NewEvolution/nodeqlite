207ee1f4e2b2ed012717fe3ab082447f4359834a
Ryan Tanay <ryantanay@newevolution.org>
2016-02-25 13:30:44 -0600

    Added express & instantiated the server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const localPort = 3000;
+const port = process.env.PORT || localPort;
+
+const express = require('express');
+const app = express();
+
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/chinook.sqlite');
 
@@ -11,3 +17,11 @@ GROUP BY Employee.EmployeeId;`, (err, res) => {
   if (err) throw err;
   console.log(res);
 });
+
+app.get('/', (req, res) => {
+  res.send('<h1>I live, I hunger</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
