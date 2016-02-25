39ba4a58138b75a873bedad604e3e19c419c73e9
Ryan Tanay <ryantanay@newevolution.org>
2016-02-25 14:00:38 -0600

    Added routes & queries for sales-by-country & sales-per-year

--- server.js
+++ server.js
@@ -9,17 +9,45 @@ const app = express();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/chinook.sqlite');
 
-db.each(`SELECT SUM(Invoice.Total) AS "Total Sales", Employee.LastName, Employee.FirstName
-FROM Customer
-JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
-JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
-GROUP BY Employee.EmployeeId;`, (err, res) => {
-  if (err) throw err;
-  console.log(res);
+app.get('/sales-by-employee', (req, res) => {
+  db.all(`SELECT SUM(Invoice.Total) AS "Total Sales", Employee.LastName, Employee.FirstName
+  FROM Customer
+  JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+  JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
+  GROUP BY Employee.EmployeeId;`, (err, data) => {
+    if (err) throw err;
+    res.send({
+      info: "Total Sales by Employee",
+      data: data
+    });
+  });
 });
 
-app.get('/', (req, res) => {
-  res.send('<h1>I live, I hunger</h1>');
+app.get('/sales-by-country', (req, res) => {
+  db.all(`SELECT SUM(Invoice.Total) AS "Total Sales", BillingCountry AS Country
+  FROM Invoice
+  GROUP BY Country
+  ORDER BY Country;`, (err, data) => {
+    if (err) throw err;
+    res.send({
+      info: "Total Sales by Country",
+      data: data
+    });
+  });
+});
+
+app.get('/sales-per-year', (req, res) => {
+  db.all(`SELECT COUNT(*) as invoices,
+  SUM(Total) as total,
+  SUBSTR(InvoiceDate, 1, 4) as year
+  FROM Invoice
+  GROUP BY year;`, (err, data) => {
+    if (err) throw err;
+    res.send({
+      info: "Total Sales Per Year",
+      data: data
+    });
+  });
 });
 
 app.listen(port, () => {
