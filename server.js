ce5a136fa20cb5b3348ac2f07ef26106cef1c7a7
Ryan Tanay <ryantanay@newevolution.org>
2016-02-25 12:06:34 -0600

    Changed query to one listing employee sales totals

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/chinook.sqlite');
 
-db.each('SELECT * FROM Employee', (err, res) => {
+db.each(`SELECT SUM(Invoice.Total) AS "Total Sales", Employee.LastName, Employee.FirstName
+FROM Customer
+JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
+GROUP BY Employee.EmployeeId;`, (err, res) => {
   if (err) throw err;
   console.log(res);
 });
