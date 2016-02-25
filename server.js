187e233adcb4bf8460ecc3a7e94c7e7085332a64
Ryan Tanay <ryantanay@newevolution.org>
2016-02-25 14:55:44 -0600

    Added query parameter handling for the totals by year, renamed returned object keys to be JSON correct

--- server.js
+++ server.js
@@ -9,8 +9,19 @@ const app = express();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/chinook.sqlite');
 
+function moneyFormat (input) {
+  const data = input.map(obj => {
+    const currencyDecimalPlaces = 2;
+    obj.total = +obj.total.toFixed(currencyDecimalPlaces)
+    return obj;
+  });
+  return data;
+}
+
 app.get('/sales-by-employee', (req, res) => {
-  db.all(`SELECT SUM(Invoice.Total) AS "Total Sales", Employee.LastName, Employee.FirstName
+  db.all(`SELECT SUM(Invoice.Total) AS "total",
+  Employee.LastName AS last,
+  Employee.FirstName AS first
   FROM Customer
   JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
   JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
@@ -18,34 +29,47 @@ app.get('/sales-by-employee', (req, res) => {
     if (err) throw err;
     res.send({
       info: "Total Sales by Employee",
-      data: data
+      data: moneyFormat(data)
     });
   });
 });
 
 app.get('/sales-by-country', (req, res) => {
-  db.all(`SELECT SUM(Invoice.Total) AS "Total Sales", BillingCountry AS Country
+  db.all(`SELECT SUM(Invoice.Total) AS "total", BillingCountry AS country
   FROM Invoice
   GROUP BY Country
   ORDER BY Country;`, (err, data) => {
     if (err) throw err;
     res.send({
       info: "Total Sales by Country",
-      data: data
+      data: moneyFormat(data)
     });
   });
 });
 
-app.get('/sales-per-year', (req, res) => {
+app.get('/sales-by-year', (req, res) => {
+  let having = '';
+  if (req.query.filter) {
+    having = 'HAVING';
+    req.query.filter.year
+      .split(',')
+      .map(y => +y)
+      .forEach(y => {
+        having += ` year = '${y}' OR`
+      }
+    );
+    having = having.substring(0, having.length - 3); // eslint-disable-line no-magic-numbers
+  }
   db.all(`SELECT COUNT(*) as invoices,
   SUM(Total) as total,
   SUBSTR(InvoiceDate, 1, 4) as year
   FROM Invoice
-  GROUP BY year;`, (err, data) => {
+  GROUP BY year
+  ${having};`, (err, data) => {
     if (err) throw err;
     res.send({
-      info: "Total Sales Per Year",
-      data: data
+      info: "Total Sales by Year",
+      data: moneyFormat(data)
     });
   });
 });
